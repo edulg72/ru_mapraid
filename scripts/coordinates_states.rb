027ce94056ea83bdc5283df69739c39a61d96b35
Eduardo Leo Garcia <eduardoleao@tre-rs.jus.br>
2016-03-09 18:12:05 -0300

    Adapted script to search the correct table for states information.

--- scripts/coordinates_states.rb
+++ scripts/coordinates_states.rb
@@ -1,9 +1,9 @@
 require 'pg'
 
 if ARGV.size > 0
-  passo = ARGV[0].to_f
+  step = ARGV[0].to_f
 else
-  passo = 0.08
+  step = 0.08
 end
 
 if ARGV.size > 1
@@ -15,35 +15,35 @@ end
 puts "#!/bin/bash\n\necho \"Start: $(date '+%d/%m/%Y %H:%M:%S')\"\n\ncase \"$3\" in"
 
 db = PG::Connection.new(:hostaddr => '127.0.0.1', :dbname => 'wazedb', :user => 'waze', :password => 'waze')
-db.prepare('box_estado','select nl_name_1 from states_shapes where (ST_Overlaps(geom,ST_SetSRID(ST_MakeBox2D(ST_Point($1,$2),ST_Point($3,$4)),4326)) or ST_Contains(geom,ST_SetSRID(ST_MakeBox2D(ST_Point($1,$2),ST_Point($3,$4)),4326))) and id_1 = $5')
+db.prepare('box_state','select nl_name_1 from states_shapes where (ST_Overlaps(geom,ST_SetSRID(ST_MakeBox2D(ST_Point($1,$2),ST_Point($3,$4)),4326)) or ST_Contains(geom,ST_SetSRID(ST_MakeBox2D(ST_Point($1,$2),ST_Point($3,$4)),4326))) and id_1 = $5')
 
-db.exec("select id_1, hasc_1, nl_name_1, ST_Xmin(ST_Envelope(geom)) as longoeste, ST_Xmax(ST_Envelope(geom)) longleste, ST_Ymax(ST_Envelope(geom)) as latnorte, ST_Ymin(ST_Envelope(geom)) as latsul from countries_tmp").each do |estado|
-  puts "# #{estado['nl_name_1']}"
-  puts "  #{estado['hasc_1']})"
-  puts "#   [#{estado['longoeste']},#{estado['latnorte']} - #{estado['longleste']},#{estado['latsul']}]"
-  latIni = (estado['latnorte'].to_f.round(2) + 0.01).round(8)
-  while latIni > estado['latsul'].to_f
-#    puts "Latitude: [#{latIni} #{(latIni - passo).round(8)}]"
+db.exec("select id_1, hasc_1, nl_name_1, ST_Xmin(ST_Envelope(geom)) as lonwest, ST_Xmax(ST_Envelope(geom)) loneast, ST_Ymax(ST_Envelope(geom)) as latnorth, ST_Ymin(ST_Envelope(geom)) as latsouth from states_shapes").each do |state|
+  puts "# #{state['nl_name_1']}"
+  puts "  #{state['hasc_1']})"
+  puts "#   [#{state['lonwest']},#{state['latnorth']} - #{state['loneast']},#{state['latsouth']}]"
+  latIni = (state['latnorth'].to_f.round(2) + 0.01).round(8)
+  while latIni > state['latsouth'].to_f
+#    puts "Latitude: [#{latIni} #{(latIni - step).round(8)}]"
     area = false
     out = ''
-    lonIni = (estado['longoeste'].to_f.round(2) - 0.01).round(8)
-    while lonIni < estado['longleste'].to_f
-#      puts "  Longitude: [#{lonIni} #{(lonIni + passo).round(8)}] #{area}"
+    lonIni = (state['lonwest'].to_f.round(2) - 0.01).round(8)
+    while lonIni < state['loneast'].to_f
+#      puts "  Longitude: [#{lonIni} #{(lonIni + step).round(8)}] #{area}"
       if area
-        if db.exec_prepared('box_estado',[lonIni, (latIni - passo).round(8), (lonIni + passo).round(8), latIni, estado['id_1']]).ntuples == 0
+        if db.exec_prepared('box_state',[lonIni, (latIni - step).round(8), (lonIni + step).round(8), latIni, state['id_1']]).ntuples == 0
           area = false
-          puts "#{out} #{lonIni} #{(latIni - passo).round(8)} #{passo}"
+          puts "#{out} #{lonIni} #{(latIni - step).round(8)} #{step}"
           out = ''
         end
       else
-        if db.exec_prepared('box_estado',[lonIni, (latIni - passo).round(8), (lonIni + passo).round(8), latIni, estado['id_1']]).ntuples > 0
+        if db.exec_prepared('box_state',[lonIni, (latIni - step).round(8), (lonIni + step).round(8), latIni, state['id_1']]).ntuples > 0
           area = true
           out = "    ruby scan_segments.rb $1 $2 #{lonIni} #{latIni}"
         end
       end
-      lonIni = (lonIni + passo).round(8)
+      lonIni = (lonIni + step).round(8)
     end
-    latIni = (latIni - passo).round(8)
+    latIni = (latIni - step).round(8)
   end
   puts "  ;;"
 end
