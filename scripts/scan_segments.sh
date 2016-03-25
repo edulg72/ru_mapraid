2472afbd7d5a9797443a3d371120b888f4bcb9c5
Eduardo Leão Garcia <edulg72@gmail.com>
2016-03-25 11:42:12 -0300

    Commented out the delete command in scan script so the segments with no area aren't removed.

--- scripts/scan_segments.sh
+++ scripts/scan_segments.sh
@@ -6757,9 +6757,9 @@ case "$3" in
     exit 1
 esac
 psql -h 127.0.0.1 -d wazedb -U waze -c 'update segments set city_id = (select gid from cities_mapraid where ST_Contains(geom, ST_SetSRID(ST_Point(segments.longitude, segments.latitude), 4326)) limit 1) where city_id is null;'
-psql -h 127.0.0.1 -d wazedb -U waze -c 'delete from segments where city_id is null;'
+#psql -h 127.0.0.1 -d wazedb -U waze -c 'delete from segments where city_id is null;'
 psql -h 127.0.0.1 -d wazedb -U waze -c 'update segments set area_id = (select id from areas_mapraid where ST_Contains(geom, ST_SetSRID(ST_Point(segments.longitude, segments.latitude), 4326)) limit 1) where area_id is null'
-psql -h 127.0.0.1 -d wazedb -U waze -c 'delete from streets where id in (select id from streets except select distinct street_id from segments);'
+#psql -h 127.0.0.1 -d wazedb -U waze -c 'delete from streets where id in (select id from streets except select distinct street_id from segments);'
 psql -h 127.0.0.1 -d wazedb -U waze -c 'update segments s1 set dc_density = (select count(*) from segments s2 where not s2.connected and s2.latitude between (s1.latitude - 0.01) and (s1.latitude + 0.01) and s2.longitude between (s1.longitude - 0.01) and (s1.longitude + 0.01)) where not s1.connected and s1.dc_density is null;'
 psql -h 127.0.0.1 -d wazedb -U waze -c "update updates set updated_at = current_timestamp where object = '$3';"
 psql -h 127.0.0.1 -d wazedb -U waze -c 'refresh materialized view vw_segments; refresh materialized view vw_streets;'
