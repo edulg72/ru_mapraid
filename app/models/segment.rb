b5ee6ac0e95529dd6ca8fac544ba603b54867d19
Eduardo Leão Garcia <edulg72@gmail.com>
2016-03-30 22:54:56 -0300

    Added check to show roads without name.

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -6,10 +6,12 @@ class Segment < ActiveRecord::Base
   belongs_to :street
   belongs_to :area, foreign_key: 'area_id',class_name: 'AreaMapraid' 
 
-  scope :drivable, -> {where('roadtype in (1,2,3,4,6,7,8,15,17,20)')}
-  scope :important, -> {where('roadtype in (3,4,6,7)')}
+  scope :drivable, -> {where(roadtype: [1,2,3,4,6,7,8,15,17,20])}
+  scope :important, -> {where(roadtype: [3,4,6,7])}
+  scope :roads, -> {where(roadtype: [3,6,7])}
   scope :disconnected, -> {where('not connected')}
   scope :no_name, -> {where('street_id is null')}
+  scope :without_name, -> { joins('left outer join vw_streets on vw_streets.id = vw_segments.street_id').where('street_id is null or vw_streets.isempty')}
   scope :with_speed, -> {where('(not fwddirection or fwdmaxspeed is not null) and (not revdirection or revmaxspeed is not null)')}
   scope :without_speed, -> {where('roadtype in (2,3,4,6,7) and ((fwddirection and fwdmaxspeed is null) or (revdirection and revmaxspeed is null))')}
   scope :unverified_speed, -> {where('(fwddirection and fwdmaxspeedunverified) or (revdirection and revmaxspeedunverified)')}
